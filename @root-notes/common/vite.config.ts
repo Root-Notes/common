ef0195d501638ae320cea23f3bc192811487c1cb
Dax Harris <matteovh@gmail.com>
2023-09-25 20:14:13 -0400

    Add base files

---
+++ @root-notes/common/vite.config.ts
@@ -0,0 +1,61 @@
+import * as path from "path";
+import typescript2 from "rollup-plugin-typescript2";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    typescript2({
+      check: false,
+      include: ["src/**"],
+      tsconfigOverride: {
+        compilerOptions: {
+          outDir: "dist",
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+      },
+      exclude: ["vite.config.ts"],
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: "src/index.ts",
+      name: "root-notes-common",
+      formats: ["es", "cjs", "umd"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that should not be bundled
+      // into your library
+      input: {
+        // @ts-ignore
+        main: path.resolve(__dirname, "src/components/main.ts"),
+      },
+      external: ["React"],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "main.css") return "my-library-react-ts.css";
+          return assetInfo.name;
+        },
+        exports: "named",
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      // @ts-ignore
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
