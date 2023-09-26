0c43706230d2a0322066292140f296ab80a5e338
Dax Harris <matteovh@gmail.com>
2023-09-26 15:16:47 -0400

    Move relevant sass to common

--- @root-notes/common/src/index.ts
+++ @root-notes/common/src/index.ts
@@ -1,4 +1,12 @@
 import { GenericIcon } from "./components/icons/GenericIcon";
 import { IconPicker } from "./components/icons/IconPicker";
+import {
+  IconRepresentation,
+  IconFamilyNames,
+  IconFamilies,
+  iconTypes,
+} from "./components/icons/types";
+import "./sass/index.scss";
 
-export { GenericIcon, IconPicker };
+export { GenericIcon, IconPicker, IconFamilyNames, iconTypes };
+export type { IconFamilies, IconRepresentation };
