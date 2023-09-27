3509996bab72eeb8a425fd327482139003b03552
Dax Harris <matteovh@gmail.com>
2023-09-27 17:07:41 -0400

    commonize project context typing

--- @root-notes/common/src/types/project.ts
+++ @root-notes/common/src/types/project.ts
@@ -17,3 +17,23 @@ export type Project = {
     records: Record[];
     manifest: ProjectManifest;
 };
+
+export interface ProjectInterface {
+    manifest: ProjectManifest;
+    records: { [key: string]: Record };
+    sync: () => Promise<void>;
+    close: () => Promise<void>;
+    createRecord?: (
+        record: Omit<Record, "id" | "lastRevision">
+    ) => Promise<Record>;
+    updateRecord?: (
+        record: Omit<Record, "id" | "lastRevision">
+    ) => Promise<Record>;
+    deleteRecord?: (recordId: string) => Promise<void>;
+    updateManifest?: (newManifest: ProjectManifest) => Promise<ProjectManifest>;
+}
+
+export type ActiveProjectContextType = {
+    active: true;
+    interface: ProjectInterface;
+};
